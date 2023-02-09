9134da5cb8d377b45c0659a57d3b2391fc94614f
Marijn Mensinga <marijn.mensinga@gmail.com>
2023-02-09 17:10:21 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export {bufferMongooseGetter, genTimestampsSchema} from './mongoose-helpers.js';
 export {toMongooseSchema} from './to-mongoose.js';
 export type {UnknownKeysHandling} from './to-mongoose.js';
 export {mongooseZodCustomType} from './zod-helpers.js';
-export {MongooseSchemaOptionsSymbol, MongooseTypeOptionsSymbol} from './extensions.js';
+export {MongooseSchemaOptionsSymbol, MongooseTypeOptionsSymbol, ZodMongoose} from './extensions.js';
